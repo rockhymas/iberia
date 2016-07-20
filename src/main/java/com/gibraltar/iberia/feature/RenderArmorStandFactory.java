08f03db871db7fd3e1486e2c2fa70c0586de47ed
Rock Hymas <rhymas@articulate.com>
2016-07-19 23:12:39 -0600

    Show elytra on armor stands

---
+++ src/main/java/com/gibraltar/iberia/feature/RenderArmorStandFactory.java
@@ -0,0 +1,13 @@
+package com.gibraltar.iberia.feature;
+
+import net.minecraftforge.fml.client.registry.IRenderFactory;
+import net.minecraft.entity.item.EntityArmorStand;
+import net.minecraft.client.renderer.entity.RenderArmorStand;
+import net.minecraft.client.renderer.entity.RenderManager;
+import net.minecraft.client.renderer.entity.Render;
+
+public class RenderArmorStandFactory implements IRenderFactory<EntityArmorStand> {
+    public Render<EntityArmorStand> createRenderFor(RenderManager manager) {
+        return new RenderArmorStandWithElytra(manager);
+    }
+}
--- src/main/java/com/gibraltar/iberia/proxy/CommonProxy.java
+++ src/main/java/com/gibraltar/iberia/proxy/CommonProxy.java
@@ -5,6 +5,7 @@ import com.gibraltar.iberia.feature.ReducedDebugInfoFeature;
 import com.gibraltar.iberia.feature.SleepToHealFeature;
 import com.gibraltar.iberia.feature.SlowGuiAccessFeature;
 import com.gibraltar.iberia.feature.QuickArmorSwapFeature;
+import com.gibraltar.iberia.feature.ArmorStandElytraRenderFeature;
 
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
@@ -17,6 +18,7 @@ public class CommonProxy {
         SleepToHealFeature.init();
         SlowGuiAccessFeature.init();
         QuickArmorSwapFeature.init();
+        ArmorStandElytraRenderFeature.init();
     }
 
 	public void init(FMLInitializationEvent event)
