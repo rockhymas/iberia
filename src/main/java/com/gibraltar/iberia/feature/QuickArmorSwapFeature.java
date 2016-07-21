06f93d5b6397734b10ff0fc39801d57702e72f7e
Rock Hymas <rhymas@articulate.com>
2016-07-20 21:14:12 -0600

    Allow elytra placement on armor stands from main hand

--- src/main/java/com/gibraltar/iberia/feature/QuickArmorSwapFeature.java
+++ src/main/java/com/gibraltar/iberia/feature/QuickArmorSwapFeature.java
@@ -11,6 +11,8 @@ import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
 import net.minecraft.inventory.EntityEquipmentSlot;
 import net.minecraft.item.ItemElytra;
+import net.minecraft.util.math.Vec3d;
+import net.minecraft.util.EnumHand;
 
 public class QuickArmorSwapFeature {
 	public static void init() {
@@ -41,6 +43,26 @@ public class QuickArmorSwapFeature {
                     ItemStack armorStandItem = armorStand.getItemStackFromSlot(slot);
                     player.setItemStackToSlot(slot, armorStandItem);
                     armorStand.setItemStackToSlot(slot, playerItem);
+                }
+            }
+            else {
+                boolean isSmall = armorStand.isSmall();
+                Vec3d vec = event.getLocalPos();
+                double d4 = isSmall ? vec.yCoord * 2.0D : vec.yCoord;
+                ItemStack stack = event.getItemStack();
+
+                if (stack == null || !(stack.getItem() instanceof ItemElytra) || event.getHand() != EnumHand.MAIN_HAND) {
+                    return;
+                }
+
+                if (d4 >= 0.9D + (isSmall ? 0.3D : 0.0D) && d4 < 0.9D + (isSmall ? 1.0D : 0.7D)) {
+                    if (armorStand.getItemStackFromSlot(EntityEquipmentSlot.CHEST) != null) {
+                        return;
+                    }
+
+                    event.setCanceled(true);
+                    armorStand.setItemStackToSlot(EntityEquipmentSlot.CHEST, stack);
+                    event.getEntityPlayer().setItemStackToSlot(EntityEquipmentSlot.MAINHAND, null);
                 }
             }
         }
