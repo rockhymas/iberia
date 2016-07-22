b45ef8a333e58e8661ddb397a03ac746416f4eeb
Rock Hymas <rhymas@articulate.com>
2016-07-22 17:35:57 -0600

    First pass at config for challenges

--- src/main/java/com/gibraltar/iberia/proxy/CommonProxy.java
+++ src/main/java/com/gibraltar/iberia/proxy/CommonProxy.java
@@ -1,5 +1,7 @@
 package com.gibraltar.iberia.proxy;
 
+import java.io.File;
+
 import com.gibraltar.iberia.feature.HardStoneFeature;
 import com.gibraltar.iberia.feature.ReducedDebugInfoFeature;
 import com.gibraltar.iberia.feature.SleepToHealFeature;
@@ -9,16 +11,57 @@ import com.gibraltar.iberia.feature.ArmorStandElytraRenderFeature;
 
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.common.config.Configuration;
+import net.minecraftforge.common.config.Property;
 
 public class CommonProxy {
+	public static Configuration config;
+	public static File configFile;
+
     public void preInit(FMLPreInitializationEvent event)
     {
-    	HardStoneFeature.init();
-        ReducedDebugInfoFeature.init();
-        SleepToHealFeature.init();
-        SlowGuiAccessFeature.init();
-        QuickArmorSwapFeature.init();
-        ArmorStandElytraRenderFeature.init();
+        configFile = event.getSuggestedConfigurationFile();
+        System.out.println(configFile);
+		config = new Configuration(configFile);
+		config.load();
+
+        Property prop = config.get("features", "Hard Stone", true);
+        boolean enabled = prop.getBoolean(true);
+        if (enabled) {
+    	    HardStoneFeature.init();
+        }
+
+        prop = config.get("features", "Reduced Debug Info", true);
+        enabled = prop.getBoolean(true);
+        if (enabled) {
+            ReducedDebugInfoFeature.init();
+        }
+
+        prop = config.get("features", "Sleep to Heal", true);
+        enabled = prop.getBoolean(true);
+        if (enabled) {
+            SleepToHealFeature.init();
+        }
+
+        prop = config.get("features", "Slow Gui Access", true);
+        enabled = prop.getBoolean(true);
+        if (enabled) {
+            SlowGuiAccessFeature.init();
+        }
+
+        prop = config.get("features", "Quick Armor Swap", true);
+        enabled = prop.getBoolean(true);
+        if (enabled) {
+            QuickArmorSwapFeature.init();
+        }
+
+        prop = config.get("features", "Elytra On Armor Stands", true);
+        enabled = prop.getBoolean(true);
+        if (enabled) {
+            ArmorStandElytraRenderFeature.init();
+        }
+
+        config.save();
     }
 
 	public void init(FMLInitializationEvent event)
