1ad3b4f4596598d8f6d1a2fedee9b0698c549d1f
Rock Hymas <rhymas@articulate.com>
2016-12-17 00:28:29 -0700

    Map compass trades to personal compass trades

--- src/main/java/com/gibraltar/iberia/challenge/FindYourWayChallenge.java
+++ src/main/java/com/gibraltar/iberia/challenge/FindYourWayChallenge.java
@@ -8,12 +8,19 @@
  */
 package com.gibraltar.iberia.challenge;
 
-import net.minecraft.item.Item;
+import net.minecraft.entity.passive.EntityVillager;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.entity.player.EntityPlayerMP;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemCompass;
+import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.village.MerchantRecipe;
+import net.minecraft.village.MerchantRecipeList;
 import net.minecraft.world.GameRules;
 import net.minecraft.world.WorldServer;
-import net.minecraft.util.math.BlockPos;
+import net.minecraftforge.event.entity.player.PlayerInteractEvent;
 import net.minecraftforge.event.entity.player.PlayerSetSpawnEvent;
 import net.minecraftforge.event.world.WorldEvent;
 import net.minecraftforge.fml.common.gameevent.PlayerEvent;
@@ -61,5 +68,71 @@ public class FindYourWayChallenge extends Challenge {
             BlockPos pos = event.player.getBedLocation(0);
             ItemPersonalCompass.setCompassSpawn(event.crafting, pos.getX(), pos.getZ());
         }
+    }
+
+    @SubscribeEvent
+    @SideOnly(Side.CLIENT)
+    public void onEntityInteract(PlayerInteractEvent.EntityInteract event) {
+        FMLLog.info("interacting with entity");
+        if (!(event.getTarget() instanceof EntityVillager)) {
+            return;
+        }
+
+        FMLLog.info("interacting with villager");
+        replaceCompassInVillagerTrades((EntityVillager)event.getTarget());
+    }
+
+    private void replaceCompassInVillagerTrades(EntityVillager villager) {
+        // get NBT data
+        NBTTagCompound data = new NBTTagCompound();
+        villager.writeEntityToNBT(data);
+
+        if (data.hasKey("Offers", 10))
+        {
+            FMLLog.info("checking villager trades");
+            NBTTagCompound offers = data.getCompoundTag("Offers");
+            MerchantRecipeList buyingList = new MerchantRecipeList(offers);
+
+            boolean modified = false;
+            for (MerchantRecipe r : buyingList) {
+                MerchantRecipe newRecipe = replaceCompassInRecipe(r);
+                if (newRecipe != null) {
+                    r.readFromTags(newRecipe.writeToTags());
+                    modified = true;
+                }
+            }
+
+            if (modified) {
+                FMLLog.info("replacing villager trades");
+                data.setTag("Offers", buyingList.getRecipiesAsTags());
+                villager.readEntityFromNBT(data);
+            }
+        }
+    }
+
+    private MerchantRecipe replaceCompassInRecipe(MerchantRecipe recipe) {
+        ItemStack buy1 = recipe.getItemToBuy();
+        ItemStack buy2 = recipe.getSecondItemToBuy();
+        ItemStack sell = recipe.getItemToSell();
+        boolean modified = false;
+
+        if (buy1 != null && buy1.getItem() instanceof ItemCompass) {
+            modified = true;
+            buy1 = new ItemStack(compassPersonal);
+        }
+        if (buy2 != null && buy2.getItem() instanceof ItemCompass) {
+            modified = true;
+            buy2 = new ItemStack(compassPersonal);
+        }
+        if (sell != null && sell.getItem() instanceof ItemCompass) {
+            modified = true;
+            sell = new ItemStack(compassPersonal);
+        }
+
+        if (modified) {
+            return new MerchantRecipe(buy1, buy2, sell);
+        }
+
+        return null;
     }
 }
